3b42f8d67752fe0cb4e9ede7c8d1182c94f724f5
endrikpartin <endrik-sander.partin@voco.ee>
2024-02-01 14:06:23 +0200

    Praktikum1

---
+++ kasutaja_loomine.ps1
@@ -0,0 +1,3 @@
+﻿$KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+
+New-LocalUser "kasutaja1" -Password $KasutajaParool -FullName "Esimene Kasutaja" -Description "Local Account - kasutaja1"
---
+++ kasutaja_loomine_1.ps1
@@ -0,0 +1,10 @@
+﻿# Defineerime vajaliku k2surea parameetrid
+param(
+ $Kasutajanimi,
+ $Taisnimi, 
+ $KontoKirjeldus
+ )
+ # Loome kasutajale parooli
+$KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+ # Lisame kasutaja vastavate andmetega 
+New-LocalUser "$Kasutajanimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$KontoKirjeldus"
